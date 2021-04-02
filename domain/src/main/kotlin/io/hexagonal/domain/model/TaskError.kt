a29d5aa13e9fac0ca468d88fec051b1ad4ff7499
Antoine Carton <>
2021-04-02 18:36:25 +0200

    :sparkles: Use arrow Either.catch function

--- domain/src/main/kotlin/io/hexagonal/domain/model/TaskError.kt
+++ domain/src/main/kotlin/io/hexagonal/domain/model/TaskError.kt
@@ -1,9 +1,9 @@
 package io.hexagonal.domain.model
 
-sealed class TaskError(open val message: String, open val e: Exception? = null) {
+sealed class TaskError(open val message: String, open val e: Throwable? = null) {
     data class InvalidState(override val message: String) : TaskError(message)
     data class NotFound(override val message: String) : TaskError(message)
     data class AlreadyExist(override val message: String) : TaskError(message)
     data class InvalidRules(override val message: String) : TaskError(message)
-    data class Unknown(override val message: String, override val e: Exception? = null) : TaskError(message, e)
+    data class Unknown(override val message: String, override val e: Throwable? = null) : TaskError(message, e)
 }
--- domain/src/main/kotlin/io/hexagonal/domain/model/extensions/EitherExtensions.kt
+++ domain/src/main/kotlin/io/hexagonal/domain/model/extensions/EitherExtensions.kt
@@ -2,29 +2,18 @@ package io.hexagonal.domain.model.extensions
 
 import arrow.core.Either
 import arrow.core.Nel
-import arrow.core.left
-import arrow.core.right
 import io.hexagonal.domain.model.DResult
 import io.hexagonal.domain.model.RuleError
 import io.hexagonal.domain.model.TaskError
 
 fun <T> Either.Companion.trying(block: () -> T): DResult<T> =
-    try {
-        block().right()
-    } catch (e: Exception) {
-        TaskError.Unknown(e.message ?: "", e).left()
-    }
+    catch { block() }
+        .mapLeft<TaskError> { TaskError.Unknown(it.message ?: "", it) }
 
-suspend fun <T> Either.Companion.sTrying(block: suspend () -> T, ifFail: (Exception) -> TaskError): DResult<T> =
-    try {
-        block().right()
-    } catch (e: Exception) {
-        ifFail.invoke(e).left()
-    }
+suspend fun <T> Either.Companion.sTrying(block: suspend () -> T, ifFail: (Throwable) -> TaskError): DResult<T> =
+    catch { block() }
+        .mapLeft { ifFail.invoke(it) }
 
 fun <T> Either.Companion.tryingNel(block: () -> T, ifFail: () -> Nel<RuleError>): Either<Nel<RuleError>, T> =
-    try {
-        block().right()
-    } catch (e: Exception) {
-        ifFail.invoke().left()
-    }
+    catch { block() }
+        .mapLeft { ifFail.invoke() }
